ef9b7eaf83b64aaef2c4df1df3e1fe051b71a7a8
ERatcliff29 <ratcliffaeric@gmail.com>
2025-03-15 19:50:24 -0600

    Add files via upload
    This Java program prints four classic jokes to the console, showcasing simple text output using System.out.println. Developed as part of an Application Programming 1 course, this project reinforces fundamental Java concepts such as string formatting and program structure while adding a touch of humor. A fun and beginner-friendly way to practice coding! 😄

---
+++ FourJokes.java
@@ -0,0 +1,21 @@
+/*
+Name: Eric Ratcliff
+Class: Application Programming 1
+Project: This program will print 4 jokes
+*/
+
+class JokingAbout {
+   public static void main(String[] args) {
+      System.out.println("Joke 1: Why don't skeletons fight each other?");
+      System.out.println("\tAnswer: Because they don't have the guts!");
+      System.out.println();
+      System.out.println("Joke 2: What did the zero say to the eight?");
+      System.out.println("\tAnswer: Nice Belt!");
+      System.out.println();
+      System.out.println("Joke 3: Why did the scarecrow win an award?");
+      System.out.println("\tAnswer: Because he was outstanding in his field!");
+      System.out.println();
+      System.out.println("Joke 4: Why don't eggs tell jokes?");
+      System.out.println("\tAnswer: Because they might crack up!"); 
+   }
+}
