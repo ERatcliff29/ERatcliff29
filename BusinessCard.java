fb16477835f147bea9c3ca3a6f4d7dfa6413e92a
ERatcliff29 <ratcliffaeric@gmail.com>
2025-03-15 19:48:44 -0600

    Add files via upload
    This simple Java program prints a formatted business card to the console. It demonstrates basic console output using System.out.println and reinforces fundamental programming concepts such as string formatting and program structure. Originally created as part of an Application Programming 1 course, this project is a great starting point for understanding Java syntax and structured output.

---
+++ BusinessCard.java
@@ -0,0 +1,16 @@
+class BusinessCard {
+
+   /*Name: Eric Ratcliff
+     Class: Application Programing 1
+     Problem: Create a business card stating your name, job, & email 
+   */
+   public static void main(String[] args) {
+   System.out.println("+--------------------------------------+");
+   System.out.println("|                                      |");
+   System.out.println("|            Eric Ratcliff             |");
+   System.out.println("|         Terminal Coordinator         |");
+   System.out.println("|         Rat4647@pennwest.edu         |");
+   System.out.println("|                                      |");
+   System.out.println("+--------------------------------------+");
+   }
+}
